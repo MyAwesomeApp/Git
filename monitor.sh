38d7b4f7a0bab97d8a73bcbb2abe0d7af6149fa8
root <root@harshita.stradsolutions.com>
2023-12-27 15:31:57 +0000

    Initial commit with system usage script

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+echo "=== CPU Load ==="
+uptime
+
+echo -e "\n=== Memory Usage ==="
+free -h
+
+echo -e "\n=== Disk Usage ==="
+df -h
+
+echo -e "\n=== Network Usage ==="
+vnstat
+
+echo -e "\n=== Active Logged-in Users ==="
+who
